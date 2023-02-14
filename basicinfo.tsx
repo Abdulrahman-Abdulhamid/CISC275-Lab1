73c851eaa24da5252f1553d6f0f94f60237d723d
Abdulrahman Abdulhamid <abdulrahman.t.abdulhamid@gmail.com>
2023-02-13 21:36:23 -0500

    Modified README.md and basicinfo.tsx

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,12 +2,11 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
-*/
+const yourName : String = "Abdulrahman Abdulhamid";
+let yourPronouns : String = "he/him";
+const yourMajor : String = "Computer Science BS";
+let classification : String = "freshman";
+let oneExcite : String = "Building a cool website";
+let anyConcerns : String = "not worried about anything";
+let funFact : String = "I am an international student from Qatar";
+
